479a72b890e82f01d12e017df0370d2cc4e91c2b
Sujeeth <sujeeth7418@gmail.com>
2026-03-11 14:54:39 +0530

    fix youtube extraction issue

--- app.py
+++ app.py
@@ -9,21 +9,19 @@ def home():
 
 @app.route('/download', methods=['POST'])
 def download():
-
     url = request.form['url']
-    quality = request.form['quality']
 
     ydl_opts = {
-        'format': f'bestvideo[height<={quality}]+bestaudio/best',
-        'merge_output_format': 'mp4',
-        'outtmpl': 'video.%(ext)s',
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
+        info = ydl.extract_info(url, download=False)
+        ydl.download([url])
         filename = ydl.prepare_filename(info)
 
     return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
